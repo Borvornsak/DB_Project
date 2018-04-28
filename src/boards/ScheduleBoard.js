23fded4633bf3961c1e9234db93a15f20c5ce714
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2018-04-28 15:31:33 +0700

    complete schedule board UI

--- src/boards/ScheduleBoard.js
+++ src/boards/ScheduleBoard.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Table } from "antd";
 
-const ScheduleBoard = () => {
-  return <div>ตรวจสอบตารางเรียน</div>;
+const ScheduleBoard = props => {
+  const title = (year, semester) => {
+    const termName = ["", "ภาคการศึกษาต้น", "ภาคการศึกษาปลาย", "ภาคฤดูร้อน"];
+    return () => (
+      <h1 style={{ whiteSpace: "pre" }}>{`${
+        termName[parseInt(semester, 4)]
+      }         ปีการศึกษา ${year}`}</h1>
+    );
+  };
+  const { year, semester } = props;
+  return (
+    <div>
+      <Table
+        columns={scheduleColumns}
+        dataSource={[]}
+        pagination={{ position: "None" }}
+        title={title(year, semester)}
+      />
+    </div>
+  );
 };
 
-export default ScheduleBoard;
+const scheduleColumns = [
+  {
+    title: "Course ID",
+    dataIndex: "courseId",
+    key: "courseId"
+  },
+  {
+    title: "Shortname",
+    dataIndex: "shortName",
+    key: "shortName"
+  },
+  {
+    title: "Time",
+    dataIndex: "time",
+    key: "time"
+  }
+];
+
+const mapStateToProps = state => {
+  const { year, semester } = state.authentication;
+  console.log(year, semester);
+  return { year, semester };
+};
+
+export default connect(mapStateToProps)(ScheduleBoard);
